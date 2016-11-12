c69912fecb1dd123110cf7d64983c461ec205987
Joey <roophanxp@gmail.com>
2016-11-12 15:23:09 -0800

    renamed variables weather

--- src/project/uiManagers/mainpagewidgets/weather.py
+++ src/project/uiManagers/mainpagewidgets/weather.py
@@ -11,96 +11,97 @@ from src.project.resources.var import *
 
 class Weather(Frame):
     def __init__(self, parent):
-        Frame.__init__(self, parent, bg='black')
+        Frame.__init__(self, parent, bg=background_color)
+
+        # Initialize text for labels
         self.temperature = ''
         self.forecast = ''
         self.location = ''
         self.currently = ''
         self.icon = ''
-        self.degreeFrm = Frame(self, bg="black")
-        self.degreeFrm.pack(side=TOP, anchor=W)
-        self.temperatureLbl = Label(self.degreeFrm, font=('Helvetica', 94), fg="white", bg="black")
-        self.temperatureLbl.pack(side=LEFT, anchor=N)
-        self.iconLbl = Label(self.degreeFrm, bg="black")
-        self.iconLbl.pack(side=LEFT, anchor=N, padx=20)
-        self.currentlyLbl = Label(self, font=('Helvetica', 28), fg="white", bg="black")
-        self.currentlyLbl.pack(side=TOP, anchor=W)
-        self.forecastLbl = Label(self, font=('Helvetica', 18), fg="white", bg="black")
-        self.forecastLbl.pack(side=TOP, anchor=W)
-        self.locationLbl = Label(self, font=('Helvetica', 18), fg="white", bg="black")
-        self.locationLbl.pack(side=TOP, anchor=W)
+
+        # Initialize color for labels
+        self.degree_frame = Frame(self, bg=background_color)  # creates a subframe so icon can be on left or right
+        self.degree_frame.pack(side=TOP, anchor=W)
+        self.temperature_label = Label(self.degree_frame, font=(font_style, 94), fg=selected_off, bg=background_color)
+        self.temperature_label.pack(side=LEFT, anchor=N)
+        self.icon_label = Label(self.degree_frame, bg=background_color)
+        self.icon_label.pack(side=LEFT, anchor=N, padx=20)
+        self.currently_label = Label(self, font=(font_style, 28), fg=selected_off, bg=background_color)
+        self.currently_label.pack(side=TOP, anchor=W)
+        self.forecast_label = Label(self, font=(font_style, 18), fg=selected_off, bg=background_color)
+        self.forecast_label.pack(side=TOP, anchor=W)
+        self.location_label = Label(self, font=(font_style, 18), fg=selected_off, bg=background_color)
+        self.location_label.pack(side=TOP, anchor=W)
         self.update()
 
     def update(self):
         print "UPDATING WEATHER INFO ON SCREEN"
-        location_obj = saved_data['location']
 
+        # Getting location related data
+        location_obj = saved_data['location']
         lat = location_obj['latitude']
         lon = location_obj['longitude']
+        location = "%s, %s" % (location_obj['city'], location_obj['region_code'])
 
-        # print "Lat : " + str(lat) + "  |  Lon : " + str(lon)
-
-        location2 = "%s, %s" % (location_obj['city'], location_obj['region_code'])
-
+        # Getting weather related data
         weather_obj = saved_data['weather']
-
         degree_sign = u'\N{DEGREE SIGN}'
-        temperature2 = "%s%s" % (str(int(weather_obj['currently']['temperature'])), degree_sign)
-        currently2 = weather_obj['currently']['summary']
-        forecast2 = weather_obj["hourly"]["summary"]
-
+        temperature = "%s%s" % (str(int(weather_obj['currently']['temperature'])), degree_sign)
+        currently = weather_obj['currently']['summary']
+        forecast = weather_obj["hourly"]["summary"]
         icon_id = weather_obj['currently']['icon']
-        icon2 = None
+        icon = None
 
+        # Looking up icon
         if icon_id in src.project.resources.lookup.icon:
-            icon2 = src.project.resources.lookup.icon[icon_id]
-
-        if icon2 is not None:
-            if self.icon != icon2:
-                self.icon = icon2
-                image = Image.open(icon2)
+            icon = src.project.resources.lookup.icon[icon_id]
+        if icon is not None:
+            if self.icon != icon:
+                self.icon = icon
+                image = Image.open(icon)
                 image = image.resize((100, 100), Image.ANTIALIAS)
                 image = image.convert('RGB')
                 photo = ImageTk.PhotoImage(image)
 
-                self.iconLbl.config(image=photo)
-                self.iconLbl.image = photo
+                self.icon_label.config(image=photo)
+                self.icon_label.image = photo
         else:
             # remove image
-            self.iconLbl.config(image='')
+            self.icon_label.config(image='')
 
-        if self.currently != currently2:
-            self.currently = currently2
-            self.currentlyLbl.config(text=currently2)
-        if self.forecast != forecast2:
-            self.forecast = forecast2
-            self.forecastLbl.config(text=forecast2)
-        if self.temperature != temperature2:
-            self.temperature = temperature2
-            self.temperatureLbl.config(text=temperature2)
-        if self.location != location2:
-            if location2 == ", ":
+        # Updating weather data
+        if self.currently != currently:
+            self.currently = currently
+            self.currently_label.config(text=self.currently)
+        if self.forecast != forecast:
+            self.forecast = forecast
+            self.forecast_label.config(text=self.forecast)
+        if self.temperature != temperature:
+            self.temperature = temperature
+            self.temperature_label.config(text=self.temperature)
+        if self.location != location:
+            if location == ", ":
                 self.location = "Cannot Pinpoint Location"
-                self.locationLbl.config(text="Cannot Pinpoint Location")
+                self.location_label.config(text="Cannot Pinpoint Location")
             else:
-                self.location = location2
-                self.locationLbl.config(text=location2)
+                self.location = location
+                self.location_label.config(text=self.location)
+
+    # -------------------------- Coloring --------------------------- #
 
     def change_color_to_yellow(self):
-        self.temperatureLbl.config(foreground="yellow")
-        self.currentlyLbl.config(foreground="yellow")
-        self.forecastLbl.config(foreground="yellow")
-        self.locationLbl.config(foreground="yellow")
+        self.temperature_label.config(foreground="yellow")
+        self.currently_label.config(foreground="yellow")
+        self.forecast_label.config(foreground="yellow")
+        self.location_label.config(foreground="yellow")
 
     def change_color_to_white(self):
-        self.temperatureLbl.config(foreground="white")
-        self.currentlyLbl.config(foreground="white")
-        self.forecastLbl.config(foreground="white")
-        self.locationLbl.config(foreground="white")
-
-    def refresh_weather_data(self):
-        self.update_weather()  # todo Check is this is right
+        self.temperature_label.config(foreground="white")
+        self.currently_label.config(foreground="white")
+        self.forecast_label.config(foreground="white")
+        self.location_label.config(foreground="white")
 
     @staticmethod
-    def convert_kelvin_to_fahrenheit(kelvin_temp):
+    def convert_kelvin_to_fahrenheit(kelvin_temp):          # todo create a settings for user!!! <-- COOL IDEA
         return 1.8 * (kelvin_temp - 273) + 32
