eb6f3a10c74cf8457e9fe67ee163f3ccc38b59b6
Joey <roophanxp@gmail.com>
2016-11-18 12:53:45 -0800

    Commented up

--- project/uiManagers/generalwidgets/returnButton.py
+++ project/uiManagers/generalwidgets/returnButton.py
@@ -7,16 +7,21 @@ from project.resources import var
 
 class ReturnButton(Frame):
     def __init__(self, parent):
+        # Gets constant variables from var file
         selected_off = var.selected_off
         background_color = var.background_color
         font_style = var.font_style
         Frame.__init__(self, parent, bg=background_color)
+
+        # initialize color for the label
         self.color_return = selected_off
+
         # initialize the return label
         self.return_button = Label(self, text="Return", font=(font_style, 28), fg=selected_off, bg=background_color)
         self.return_button.pack(side=LEFT, anchor=N)
 
-    def change_color_all(self,mode):
-        if self.color_return != mode:
+    # Changes color based on specified mode
+    def change_color_all(self, mode):
+        if self.color_return != mode:  # will only act if the requested change is different
             self.color_return = mode
             self.return_button.config(foreground=self.color_return)
--- project/uiManagers/generalwidgets/settingsbutton.py
+++ project/uiManagers/generalwidgets/settingsbutton.py
@@ -5,31 +5,41 @@ from project.resources import var
 
 class SettingsButton(Frame):
     def __init__(self, parent):
+        # keep an instance of the constant variables in var file
+        # (just so you don't need to continually say var._____)
         selected_off = var.selected_off
         background_color = var.background_color
         font_style = var.font_style
         Frame.__init__(self, bg=background_color)
+
+        # to keep track of current color
         self.settings_selected = selected_off
 
+        # initialize the 2 possible images
+        # Settings - white
         image = Image.open("assets/settings.png")  # todo DOesnt WOrk! FIX THIS
         image = image.resize((50, 50), Image.ANTIALIAS)
         image = image.convert('RGB')
         self.photo_not_selected = ImageTk.PhotoImage(image)
 
+        # Settings - Yellow
         image = Image.open("assets/settings_on.png")  # todo DOesnt WOrk! FIX THIS
         image = image.resize((50, 50), Image.ANTIALIAS)
         image = image.convert('RGB')
         self.photo_selected = ImageTk.PhotoImage(image)
 
+        # Packs in the icon
         self.icon_label = Label(self, bg=background_color, image=self.photo_not_selected)
         self.icon_label.image = self.photo_not_selected
         self.icon_label.pack(side=LEFT, anchor=N)
 
-        self.event_name = "Settings"
-        self.event_name_label = Label(self, text=self.event_name, font=(font_style, 24), fg=selected_off,
+        # Adds in "settings label"
+        self.settings_text = "Settings"
+        self.settings_text_label = Label(self, text=self.settings_text, font=(font_style, 24), fg=selected_off,
                                       bg=background_color)
-        self.event_name_label.pack(side=LEFT, anchor=CENTER)
+        self.settings_text_label.pack(side=LEFT, anchor=CENTER)
 
+    # Changes color based on passed in mode
     def change_color_setting(self, mode):
         if self.settings_selected != mode:
             self.settings_selected = mode
@@ -39,6 +49,6 @@ class SettingsButton(Frame):
             else:
                 self.icon_label.config(image=self.photo_not_selected)
                 self.icon_label.image = self.photo_not_selected
-            self.event_name_label.config(foreground=self.settings_selected)
+            self.settings_text_label.config(foreground=self.settings_selected)
 
 
