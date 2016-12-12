0ecc537c095e72e6c0f5639af1c0cf21b7a6e3df
Joey <roophanxp@gmail.com>
2016-12-12 14:42:16 -0800

    testing

--- project/testDistanceSensor.py
+++ project/testDistanceSensor.py
@@ -1,18 +1,13 @@
 import sys
 
 sys.path.append("../..")
-from project.hardwareManagers.camera import *
-from project.hardwareManagers.powerswitch import *
+# from project.hardwareManagers.camera import *
+# from project.hardwareManagers.powerswitch import *
 from project.hardwareManagers.ultrasoundhandler import *
-from project.uiManagers.uihandler import *
-from project.resources import var
+# from project.uiManagers.uihandler import *
+# from project.resources import var
 
 ultra_sound_handler = UltraSoundHandler()
-power_switch = PowerSwitch()
-camera = Camera(var.wall_light_color)
-ui_manager = UIManager()
-print var.other_data
-if var.other_data['manual_mode']:
-    camera.turn_off()   # todo add onto this . close windows?
+
 while True:
     ultra_sound_handler.wait_to_start_up()
