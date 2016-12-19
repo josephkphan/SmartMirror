f469f5bde29e4b651c197af2e12101319fcb6bc2
Joey <roophanxp@gmail.com>
2016-12-19 11:31:31 -0800

    added setup.py file to install all libraries except OpenCV and RPIO

---
+++ setup.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python
+
+import os
+
+# What your libraries your system should already have:
+#   - math
+#   - json
+#   - time
+#   - Datetime
+#   - traceback
+
+# If you want to test. run in terminal 'python'
+# type in import [LIBRARY NAME]
+# if you see just 
+# >
+# then it found an existing library
+
+# Install Shapely
+os.system("sudo apt-get install python-shapely")
+
+# Install PIL
+os.system("sudo apt-get install python-imaging-tk")
+os.system("sudo apt-get install python-imaging")
+
+# Install enum
+os.system("sudo apt-get install python-enum")
+
+# Install numpy
+os.system("sudo apt-get install python-numpy")
+
+# Install feedparser
+os.system("sudo apt-get install python-feedparser")
+
+# Install requests
+os.system("sudo apt-get install python-requests")
+
+# Install RPI.GPIO
+# .... TODO FINISH LATER     https://goo.gl/QpUyRZ
+
+# Install openCV
+# .... TODO FINISH LATER
+
+
+# ------------------- WHAT EVERY USER NEEDS TO DO --------------------- #
+# Register for a Weather API Key at https://darksky.net/dev/ and store the key 
+# in a key.txt file
+
