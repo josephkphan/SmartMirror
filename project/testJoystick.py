05eba4c6b8498a544f30f5db035a4f5d8b71277a
thomasvn <thomasnguyen96@gmail.com>
2017-01-23 16:44:41 -0800

    Fixed bug that would disregard the 'pressed' option

--- project/testJoystick.py
+++ project/testJoystick.py
@@ -19,8 +19,17 @@ while True:
 
     # Hold down enter on Joystick to turn off
     while joystick_state == 'pressed':
+
+        # Did not hold for 5 seconds
+        if joystick_state != 'pressed':
+            joystick_state = 'pressed'
+            break
+
+        # Update the direction of the joystick
         joystick_state = joystick_manager.get_direction()
         end = time.time()
+
+        # Wait for 5 seconds to toggle the mirror
         if (end-start) >= 5:
             joystick_state = 'toggled'
             break
