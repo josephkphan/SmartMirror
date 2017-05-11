421523a575a75c91aeff536e9bf20033dc88df6d
thomasvn <thomasnguyen96@gmail.com>
2017-05-11 11:21:54 -0700

    Proper coloring and font size changing in the To Do List

--- project/uimanagers/plannerwidgets/todolist.py
+++ project/uimanagers/plannerwidgets/todolist.py
@@ -19,37 +19,38 @@ class ToDoList(Frame):
         # Initializing a color boolean for all labels
         self.color_all = selected_off
 
+        # TODO: Create a json for all todo items
         # Initialize to do JSON data
-        self.to_do_list_string = {"Clean the house", "Work on the smart mirror", "Wash my clothes"}
+        self.to_do_list_string = ["Clean the house", "Work on smart mirror", "Wash my clothes", "Buy a Car", "Get your shit together"]
         self.to_do_list = []
 
         # Initializing Labels
+        self.to_do_title_label = Label(self.container, text="To Do List", font=(font_style, font_sizes['big']),
+                                       fg=selected_off, bg=background_color)
+        self.to_do_title_label.pack(side=TOP, anchor=N)
         for item in self.to_do_list_string:
+            print (item)
             self.to_do_label = Label(self.container, text=item, font=(font_style, font_sizes['text']),
                                      fg=selected_off, bg=background_color)
-            self.to_do_label.pack(side=BOTTOM, anchor=N)
+            self.to_do_label.pack(side=TOP, anchor=N)
             self.to_do_list.append(self.to_do_label)
-        self.to_do_title_label = Label(self.container, text="To Do list", font=(font_style, font_sizes['title']),
-                                       fg=selected_off, bg=background_color)
-        self.to_do_title_label.pack(side=BOTTOM, anchor=N)
-
-        # TODO: Color all each individual item in the to do list
-        # TODO: Create check boxes for the todo list
-        # TODO: Dynamically load elements into todolist
 
     def update_now(self):
-        print("We just Updated")
+        self.update_font_size()
+        #TODO: Remake the todo list based on online updates
 
     def change_color_all(self, mode):
         if self.color_all != mode:
             self.color_all = mode
             self.to_do_title_label.config(foreground=self.color_all)
-            # for item in self.to_do_list:
-            #     item.config(foreground=self.color_all)
+            for to_do_item in self.to_do_list:
+                to_do_item.config(foreground=self.color_all)
 
     def change_color_todolist(self, mode, current_zone):
         if self.color_all != mode:
             self.color_all = mode
+            if current_zone == zone.PlannerPage.todolist:
+                self.to_do_title_label.config(foreground=self.color_all)
             if current_zone == zone.PlannerPage.todo0:
                 self.to_do_list[0].config(foreground=self.color_all)
             if current_zone == zone.PlannerPage.todo1:
@@ -61,3 +62,7 @@ class ToDoList(Frame):
             if current_zone == zone.PlannerPage.todo4:
                 self.to_do_list[4].config(foreground=self.color_all)
 
+    def update_font_size(self):
+        self.to_do_title_label.config(font=(var.font_style, var.font_sizes['big']))
+        for to_do_item in self.to_do_list:
+            to_do_item.config(font=(var.font_style, var.font_sizes['text']))
