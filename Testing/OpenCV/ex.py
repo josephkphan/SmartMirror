764b444fea446bbac889391ac02261dd47f92cbc
Joey Phan <joseph.phan@whitehatsec.com>
2018-02-23 23:09:27 -0800

    moved testcode into its own branch

--- Testing/OpenCV/ex.py
+++
@@ -1,9 +0,0 @@
-k = 5
-if k == 1 or k == 2:
-    j = 2 * k - 1
-if k == 3 or k == 5:
-    k = 3 * k + 1
-if k == 4:
-    j = 4 * k - 1
-if k==6 or k == 7 or k == 8:
-    j = k - 2
--- Testing/stock/testingurl.py
+++
@@ -1,6 +0,0 @@
-import urllib2
-import time
-
-urlToVisit = 'http://chartapi.finance.yahoo.com/instrument/1.0/AAPL/chartdata;type=quote;range=10d/csv'
-sourceCode = urllib2.urlopen(urlToVisit).read().decode()
-print sourceCode
